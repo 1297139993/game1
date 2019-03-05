7603cfb55e63f376fe12366be7c9a653a92dd2b6
pcx123 <1297139993@qq.com>
2019-03-05 20:47:45 +0800

    分支dev的测试提交重复代码

---
+++ testdev/Born.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Born : MonoBehaviour {
+    public GameObject Player;
+    public GameObject[] Enemy;
+    public bool createPlayer;
+	// Use this for initialization
+	void Start () {
+        Invoke("BornTank",1.5f);
+        Destroy(gameObject, 1.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void BornTank()
+    {
+        if (createPlayer)
+        {
+            Instantiate(Player, transform.position, Quaternion.identity);
+        }
+        else
+        {
+            int num = Random.Range(0, 2);
+            Instantiate(Enemy[num], transform.position, Quaternion.identity);
+        }
+    }
+}
---
+++ testdev/PlayManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayManager : MonoBehaviour {
+    public int lifeValue = 3;
+    public int playerScore = 0;
+    public bool dead;
+    public GameObject born;
+    public bool isDefeat;
+    public Text PlayerScore;
+    public Text PlayerLifeValue;
+    public GameObject isDefeatUI;
+    private static PlayManager instance;
+
+    public static PlayManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+        set
+        {
+            instance = value;
+        }
+    }
+    private void Awake()
+    {
+        Instance = this;
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	// Update is called once per frame
+	void Update () {
+        if (isDefeat)
+        {
+            isDefeatUI.SetActive(true);
+            Invoke("ReturnMainMenu", 2);
+            
+        }
+        if (dead)
+        {
+            Recover();
+        }
+        PlayerScore.text = playerScore.ToString();
+        PlayerLifeValue.text = lifeValue.ToString();
+    }
+    private void Recover()
+    {
+        if (lifeValue<=0)
+        {
+            isDefeat = true;
+            Invoke("ReturnMainMenu", 2);
+            //游戏失败返回界面
+        }
+        else
+        {
+            lifeValue--;
+            GameObject go = Instantiate(born, new Vector3(-2, -8, 0), Quaternion.identity);
+            go.GetComponent<Born>().createPlayer = true;
+            dead = false;
+        }
+    }
+    private void ReturnMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
